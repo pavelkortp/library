ed8f34bd959835fb12affb46257a0d6b4953cb57
Pavlo <pavelkortp@gmail.com>
2023-11-19 00:16:34 +0200

    download ejs

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express, {Express} from 'express';
 export const app: Express = express();
 const PORT = 3000;
 
+app.set('views', express.static('views'));
+app.set('view engine', 'ejs');
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 })
