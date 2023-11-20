80bede21406cdcb4bdd903aa2aa5b99938117099
Pavlo <pavelkortp@gmail.com>
2023-11-21 00:50:42 +0200

    adds db-config and update server, install mysql

---
+++ db-config.ts
@@ -0,0 +1,14 @@
+/**
+ * Db host.
+ */
+export const host: string = '';
+
+/**
+ * Db user.
+ */
+export const user: string = '';
+
+/**
+ * Db user's pass.
+ */
+export const password: string = '';
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import express, {Express} from 'express';
 import {booksRouter} from './routes/books-router.js';
+import mysql from 'mysql';
+import {user, password, host} from '../db-config.js';
+
 
 export const app: Express = express();
 const PORT = 3000;
+
+
+const con = mysql.createConnection({
+  host: host,
+  user: user,
+  password: password
+});
+
+(async ()=>{
+    con.connect((err:Error)=>{
+        console.log(err);
+    })
+})();
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
