5ae77d1baef1d98b1cfeb0c17c0c6c5920f62180
Pavlo <pavelkortp@gmail.com>
2023-11-21 01:16:36 +0200

    creates succsessful connection to db

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,27 @@
-import express, {Express} from 'express';
-import {booksRouter} from './routes/books-router.js';
-import mysql from 'mysql';
-import {user, password, host} from '../db-config.js';
+import express, { Express } from 'express';
+import { booksRouter } from './routes/books-router.js';
+import mysql, { Connection } from 'mysql2';
+import { user, password, host, database } from './db-controls/db-config.js';
 
 
 export const app: Express = express();
 const PORT = 3000;
 
 
-const con = mysql.createConnection({
-  host: host,
-  user: user,
-  password: password
+export const con: Connection = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database
 });
 
-(async ()=>{
-    con.connect((err:Error)=>{
-        console.log(err);
-    })
+(async () => {
+    try {
+        await con.connect();
+        console.log('Connected to the database');
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
+    }
 })();
 
 app.set('view engine', 'ejs');
