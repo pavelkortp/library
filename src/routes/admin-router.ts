07376f20f1d81b1be243103701b4465beeeed803
Pavlo <pavelkortp@gmail.com>
2023-11-22 00:58:08 +0200

    optimized admin router

--- src/routes/admin-router.ts
+++ src/routes/admin-router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getBooksTable, createBook } from '../controllers/admin-controller.js';
-// import  from '../controllers/admin-controller.js';
 
 export const adminRouter = express.Router();
 
-adminRouter.get('/admin/api/v1/', getBooksTable);
-adminRouter.post('/admin/api/v1/', createBook)
+adminRouter
+    .route('/admin/api/v1/')
+    .get(getBooksTable)
+    .post(createBook);
+
