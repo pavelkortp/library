6378696247d3c1c1046412660eb73040ef888274
Pavlo <pavelkortp@gmail.com>
2023-11-21 02:18:32 +0200

    adds create book scrypt

--- src/db-controls/db-scrypst.ts
+++ src/db-controls/db-scrypst.ts
@@ -1,5 +1,7 @@
-import { Connection } from 'mysql2';
+import mysql, { Connection } from 'mysql2';
 import { readFile } from 'fs/promises';
+import { BookModel } from '../models/book-model.js';
+import { con } from '../server.js';
 
 /**
  * Connects to db.
@@ -30,4 +32,18 @@ export const createTables = async (con: Connection) => {
 const createTable = async (con: Connection, scryptPath: string) => {
     const q = await readFile(scryptPath, 'utf-8');
     con.query(q);
+}
+/**
+ * Creates new book in books table ----- NOT TESTED
+ * @param book 
+ */
+export const createBook = async (book: BookModel) => {
+    const sqlQuery = await readFile('src/db-controls/sql/create-authors-table.sql', 'utf-8');
+    const name = mysql.escape(book.name);
+    const description = mysql.escape(book.description);
+    const year = mysql.escape(book.year);
+    const language = mysql.escape(book.language);
+    const art = mysql.escape(book.art);
+
+    con.query(sqlQuery, [name, description, year, language, art]);
 }
---
+++ src/db-controls/sql/create-book.sql
@@ -0,0 +1,1 @@
+INSERT INTO books (?, ?, ?, ?, ?) VALUES (${name}, ${description}, ${year}, ${language}, ${art})
