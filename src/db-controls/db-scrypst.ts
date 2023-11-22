1932b01b2de913c9af2654c0eac3c17dc9d0827d
Pavlo <pavelkortp@gmail.com>
2023-11-22 18:32:33 +0200

    adds new layer to work with db - repository

--- src/db-controls/db-scrypst.ts
+++ src/db-controls/db-scrypst.ts
@@ -1,4 +1,4 @@
-import mysql, { Connection } from 'mysql2/promise';
+import mysql, { Connection, RowDataPacket } from 'mysql2/promise';
 import { readFile } from 'fs/promises';
 import { BookModel } from '../models/book-model.js';
 import { con } from '../server.js';
@@ -33,17 +33,46 @@ const createTable = async (con: Connection, scryptPath: string) => {
     const q = await readFile(scryptPath, 'utf-8');
     con.query(q);
 }
+
+/**
+ * Returns all teble's entrys
+ * @returns book array
+ */
+export const getAllBooks = async (): Promise<BookModel[]> => {
+    const q = await readFile('src/db-controls/sql/get-all-books.sql', 'utf-8');
+    const [rows] = await con.execute<RowDataPacket[]>(q);
+
+    return rows.map((e: any) => new BookModel(
+        e.name,
+        e.year,
+        'without author', //
+        e.language,
+        e.description,
+        e.art,
+        e.id,
+        e.pages,
+        e.views,
+        e.clicks
+    ));
+}
+
 /**
  * Creates new book in books table ----- NOT TESTED
  * @param book 
  */
 export const createBook = async (book: BookModel) => {
-    const sqlQuery = await readFile('src/db-controls/sql/create-authors-table.sql', 'utf-8');
-    const name = mysql.escape(book.name);
-    const description = mysql.escape(book.description);
-    const year = mysql.escape(book.year);
-    const language = mysql.escape(book.language);
-    const art = mysql.escape(book.art);
+    
+    const sqlQuery = await readFile('src/db-controls/sql/create-book.sql', 'utf-8');
+    const values = [
+        book.name,
+        book.description,
+        book.year,
+        book.language,
+        book.art,
+        book.rating,
+        book.views,
+        book.clicks,
+    ];
 
-    con.query(sqlQuery, [name, description, year, language, art]);
+    con.execute(sqlQuery, values);
 }
---
+++ src/repositories/books-repository.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { BookModel } from '../models/book-model.js';
+import { readFile } from 'fs/promises';
+import { con } from '../server.js';
+import { createBook } from '../db-controls/db-scrypst.js';
+
+
+/**
+ * Searches in db books with year and return it.
+ * @param year book's realese year
+ * @returns found books.
+ */
+export const findByYear = async (year: number): Promise<BookModel[]> => {
+    const q: string = await readFile('src/db-controls/sql/get-books-by-year.sql', 'utf-8');
+    const [rows] = await con.execute(q, [year]);
+
+    return [];
+
+}
+
+/**
+ * Searches in db book with name and return it.
+ * @param name book's name.
+ * @returns found book.
+ */
+export const findByName = async (name: string) => {
+    const q: string = await readFile('src/db-controls/sql/get-book-by-name.sql', 'utf-8');
+    const r = await con.execute(q, [name]);
+    // return new BookModel();
+}
+
+/**
+ * Satored book in db.
+ * @param book new book.
+ */
+export const save = async (book: BookModel): Promise<void> => {
+    await createBook(book);
+}
