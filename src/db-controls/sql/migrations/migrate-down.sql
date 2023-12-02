05b4dd6d18a529d1db4515dcdb207e5da532aba9
Pavlo <pavelkortp@gmail.com>
2023-12-02 20:26:27 +0200

    update down migration

---
+++ src/db-controls/sql/migrations/migrate-down.sql
@@ -0,0 +1,10 @@
+ALTER TABLE books
+ADD COLUMN author varchar(100);
+
+UPDATE books
+JOIN books_authors ON books.id = books_authors.book_id
+JOIN authors ON books_authors.author_id_1 = authors.id
+SET books.author = authors.name;
+
+DROP TABLE authors;
+DROP TABLE books_authors;
