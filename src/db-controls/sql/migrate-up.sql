a5d7d8349c950349dde74a1da2c97bd69fb33f61
Pavlo <pavelkortp@gmail.com>
2023-12-02 19:33:30 +0200

    adds migration up

---
+++ src/db-controls/sql/migrate-up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS library.authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO library.authors (name)
+SELECT DISTINCT author FROM library.books;
+
+
+CREATE TABLE IF NOT EXISTS library.books_authors(
+    book_id INT,
+    author_id_1 INT NULL,
+    author_id_2 INT NULL,
+    author_id_3 INT NULL,
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+INSERT INTO library.books_authors(books_id, author_id_1)
+SELECT authors.id
+FROM library.books
+RIGHT JOIN library.authors
+ON books.author = authors.name
+ORDER BY books.id;
