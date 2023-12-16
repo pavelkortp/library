cca0607d5446e23271dad004e754bab5d581bd40
Pavlo <pavelkortp@gmail.com>
2023-12-17 00:12:39 +0200

    Code refactor

--- src/migrator/migrator.ts
+++ src/migrator/migrator.ts
@@ -3,7 +3,6 @@ import {readFile, writeFile} from 'fs/promises';
 import {connection} from '../config/db-connection.js';
 
 
-
 /**
  * Current db version:<br>
  * <b>V1</b> - only books table and only one author.<br>
@@ -11,6 +10,15 @@ import {connection} from '../config/db-connection.js';
  * tables (From books removes author column).
  */
 let version: 'v1' | 'v2' = JSON.parse(await readFile('src/config/db-version.json', 'utf-8')).version;
+
+/**
+ * Returns the sql query by name.
+ * @param name name of sql file.
+ * @param version what version of the migration it belongs to.
+ */
+const getSql = async (name: string, version: 'up' | 'down' = 'up'): Promise<string> => {
+    return await readFile(`src/sql/migrations/${version}/${name}.sql`, 'utf-8');
+};
 
 /**
  * Update db structure.
@@ -26,7 +34,7 @@ const migrateUp = async () => {
         JSON.stringify({version: 'v2'})
     )
     version = 'v2';
-}
+};
 
 /**
  * Downgrade db structure.
@@ -41,23 +49,29 @@ const migrateDown = async () => {
         JSON.stringify({version: 'v1'})
     )
     version = 'v1';
-}
+};
 
 const createAuthorsTable = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/up/create-authors-table.sql`, 'utf-8');
+    const sql: string = await getSql('create-authors-table');
     await connection.execute(sql);
 };
 
 const getAuthorsColumn = async (): Promise<string[]> => {
-    const sql: string = await readFile(`src/sql/migrations/up/get-authors.sql`, 'utf-8');
+    const sql: string = await getSql('get-authors');
     const [rows] = await connection.query<RowDataPacket[]>(sql);
     return rows.map((e) => e.author + '');
-}
+};
 
+/**
+ * Inserts into authors table all unique authors, <br>
+ * that occur in the author field of the books table.
+ */
 const fillAuthorsTable = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/up/fill-authors-table.sql`, 'utf-8');
+    const sql: string = await getSql('fill-authors-table');
 
+    // In author field can be more than one author
     const authors = await getAuthorsColumn();
+
     const a: string[][] = authors.map((e) => e.split(',').map((name) => name.trim()));
 
     const names = new Set<string>(([] as string[]).concat(...a));
@@ -68,20 +82,27 @@ const fillAuthorsTable = async (): Promise<void> => {
 };
 
 const createBooksAuthorsTable = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/up/create-books_authors-table.sql`, 'utf-8');
+    const sql: string = await getSql('create-books_authors-table');
     await connection.execute(sql);
 };
 
+/**
+ * Fills the relationship table with book id and author id records.
+ */
 const fillBooksAuthorsTable = async (): Promise<void> => {
-    const insertValues: string = await readFile(`src/sql/migrations/up/fill-books_authors-table.sql`, 'utf-8');
+    const insertValues: string = await getSql('fill-books_authors-table');
 
-    const getBooks: string = await readFile(`src/sql/migrations/up/get-books.sql`, 'utf-8');
+    const getBooks: string = await getSql('get-books');
     const [books] = await connection.query<RowDataPacket[]>(getBooks);
 
-    const getAuthors: string = await readFile(`src/sql/migrations/up/get-authors-table.sql`, 'utf-8');
+    const getAuthors: string = await getSql('get-authors-table');
     const [authors] = await connection.query<RowDataPacket[]>(getAuthors);
 
     for (const book of books) {
+        /* Book in filed author can have a few authors,
+         * so we need separate authors
+         * for creating different records.
+         */
         const bookAuthors: string[] = book.author.split(',');
         const bookId: number = book.id;
         const authorsIds: number[] = bookAuthors
@@ -90,6 +111,7 @@ const fillBooksAuthorsTable = async (): Promise<void> => {
                     return entry.name.trim() == name.trim()
                 })!.id);
 
+        /* Insert into table entry for each author id. */
         for (const authorId of authorsIds) {
             await connection.execute(insertValues, [bookId, authorId]);
         }
@@ -97,29 +119,29 @@ const fillBooksAuthorsTable = async (): Promise<void> => {
 };
 
 const removeAuthorColumnFromBooks = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/up/remove-column.sql`, 'utf-8');
+    const sql: string = await getSql('remove-column');
     await connection.execute(sql);
 };
 
 const addAuthorColumnToBooks = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/down/add-column.sql`, 'utf-8');
+    const sql: string = await getSql('add-column', 'down');
     await connection.execute(sql);
-}
+};
 
 const fillAuthorsColumn = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/down/fill-authors-column.sql`, 'utf-8');
+    const sql: string = await getSql('fill-authors-column', 'down');
     await connection.execute(sql);
-}
+};
 
 const removeBooksAuthorsTable = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/down/remove-books_authors.sql`, 'utf-8');
+    const sql: string = await getSql('remove-books_authors', 'down');
     await connection.execute(sql);
-}
+};
 
 const removeAuthorsTable = async (): Promise<void> => {
-    const sql: string = await readFile(`src/sql/migrations/down/remove-authors.sql`, 'utf-8');
+    const sql: string = await getSql('remove-authors', 'down');
     await connection.execute(sql);
-}
+};
 
 export const migrator = {
     up: async function () {
@@ -141,5 +163,5 @@ export const migrator = {
         }
     },
     version: version
-}
+};
 
