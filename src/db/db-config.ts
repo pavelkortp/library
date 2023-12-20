143fc22e91e322fd1c0b28558537cd5cabeb6b1c
Pavlo <pavelkortp@gmail.com>
2023-12-20 21:52:50 +0200

    adds db-config

---
+++ src/db/db-config.ts
@@ -0,0 +1,19 @@
+/**
+ * Db host.
+ */
+export const host: string = 'localhost';
+
+/**
+ * Db user.
+ */
+export const user: string = 'your_name';
+
+/**
+ * Database name
+ */
+export const database: string = 'your_db';
+
+/**
+ * Db user's pass.
+ */
+export const password: string = 'your_pass';
